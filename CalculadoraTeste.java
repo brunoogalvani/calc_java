1e1ec3063c32d65c38e25084441dffd0d28dc87b
Bruno Galvani <bthezolin@gmail.com>
2024-03-25 21:07:30 -0300

    apostila(git-java): teste da funcao somar

---
+++ CalculadoraTeste.java
@@ -0,0 +1,8 @@
+public class CalculadoraTeste {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        //testar soma
+        double soma = calc.somar(50, 100);
+        System.out.println(soma);
+    }
+}
