cec19daf34387923c673967e96a77e3710ab61b2
Bruno Galvani <bthezolin@gmail.com>
2024-03-26 21:06:53 -0300

    apostila(git-java): funcao numero preferido implementada

--- CalculadoraTeste.java
+++ CalculadoraTeste.java
@@ -1,11 +1,14 @@
+import javax.swing.JOptionPane;
+
 public class CalculadoraTeste {
     public static void main(String[] args) {
-        Calculadora calc = new Calculadora();
+        Calculadora calc = new Calculadora(Double.parseDouble(JOptionPane.showInputDialog("Digite seu número preferido:")));
         //testar soma
         double soma = calc.somar(2, 3);
         System.out.println(soma);
         //testar substracao
         double sub = calc.subtrair(2, 3);
         System.out.println(sub);
+        System.out.println(calc.somarComPreferido(Double.parseDouble(JOptionPane.showInputDialog("Digite seu número para somar com o preferido:"))));
     }
 }
